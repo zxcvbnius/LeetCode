b7bf3e1f044547203e95e26fc211f02b5b9a3351
zxcvbnius <zxcvbnius@gmail.com>
2016-10-28 11:33:42 +0800

    Create 271_ContainsDuplicate(Bad).java

---
+++ algorithm/271_ContainsDuplicate(Bad).java
@@ -0,0 +1,17 @@
+/**
+ * Given a non-empty array of integers, return the third maximum number in this array. 
+ * If it does not exist, return the maximum number. The time complexity must be in O(n).
+ * Result. "Your runtime beats 21.82% of java submissions :("
+ */
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+    	int len = nums.length;
+    	for(int i = 0 ; i < len; i++ ) {
+    		int val = nums[i];
+    		if(map.get(val) != null) return true;
+    		else map.put(val, i);
+    	}
+        return false;
+    }
+}
