320363cfc9e6be6b9d34174af206d2b16596b2a6
zxcvbnius <zxcvbnius@gmail.com>
2016-10-28 11:44:38 +0800

    Create 271_ContainsDuplicate(Good).java

---
+++ algorithm/271_ContainsDuplicate(Good).java
@@ -0,0 +1,17 @@
+/**
+ * Given a non-empty array of integers, return the third maximum number in this array. 
+ * If it does not exist, return the maximum number. The time complexity must be in O(n).
+ * Result. "Your runtime beats 85.60% of java submissions :)"
+ * Time complexity : O(NlogN), Space complexity : O(n)
+ */
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int ind = 1; ind < nums.length; ind++) {
+            if(nums[ind] == nums[ind - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
