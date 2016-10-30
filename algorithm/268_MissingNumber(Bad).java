42b75c90638f9b2685298e7f15345ce6b43173b2
zxcvbnius <zxcvbnius@gmail.com>
2016-10-31 02:11:24 +0800

    Update 268_MissingNumber(Bad).java

--- algorithm/268_MissingNumber(Bad).java
+++ algorithm/268_MissingNumber(Bad).java
@@ -17,3 +17,29 @@
         else return sum - count; 
     }
 }
+
+// Another solutions with the wrost time complexity O(logN)
+// "Your runtime beats 11.67% of java submissions."
+// (1)
+public class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0 ; i < nums.length; i++) {
+            if(nums[i] != i) return i;
+        }        
+        return nums.length;
+    }
+}
+
+// (2) XOR, having the wrost time complexity too
+// "Your runtime beats 11.67% of java submissions." 
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int res = nums.length;
+        for(int i = 0 ; i < nums.length; i++) {
+            res ^= i;
+            res ^= nums[i];
+        }        
+        return res;
+    }
+}
