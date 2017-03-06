24542ff3b926ef5cb0beb50f99cfffb6adfa1296
zxcvbnius <zxcvbnius@gmail.com>
2017-03-05 23:07:50 -0500

    Update 530_MinimumAbsolute DifferenceinBST.java

--- algorithm/530_MinimumAbsolute DifferenceinBST.java
+++ algorithm/530_MinimumAbsolute DifferenceinBST.java
@@ -42,3 +42,23 @@
         parents.remove(root);
     }
 }
+
+// Inorder travel
+ public class Solution {
+    TreeNode pre = null;
+    int min = Integer.MAX_VALUE;
+    public int getMinimumDifference(TreeNode root) {
+        inOrder(root);
+        return min;
+    }
+    
+    private void inOrder(TreeNode root) {
+        if (root == null)   return;
+        inOrder(root.left);
+        if (pre != null) {
+            min = Math.min(min, root.val - pre.val);
+        }
+        pre = root;
+        inOrder(root.right);
+    }
+}
