f4fb8d1b06432ae1fb2f726c6315703d51dc1ff2
zxcvbnius <zxcvbnius@gmail.com>
2019-07-28 01:11:11 +0800

    feat: 136_Single_Number

---
+++ 2019-practices/136_Single_Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int len = nums.length;
+        int result = 0;
+        for(int i = 0 ; i < len ; i++ ) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}
