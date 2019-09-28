3197df205bd76186e52a47c032dc0b1e6056037e
zxcvbnius <zxcvbnius@gmail.com>
2019-09-28 10:30:08 +0800

    updated: 849. Maximize Distance to Closest Person

--- 2019-practices/833_Find_And_Replace_in_String.java
+++ 2019-practices/833_Find_And_Replace_in_String.java
@@ -1,42 +1,24 @@
 class Solution {
-    public String findReplaceString(String S, int[] indexes, String[] sources, String[] targets) {
-        boolean[] replace = new boolean[ sources.length ];
-        
-        char[] sChar = S.toCharArray();
-        HashMap<Integer, Integer> replaces = new HashMap<>(); // index of schar, index of indexes array
-        
-        for(int i = 0 ; i < indexes.length ; i++) {
-            char[] sourceChars = sources[i].toCharArray();
-            int sPos = indexes[i];
-            int offset = 0;
-            
-            boolean isReplaced = true;
-            while( sPos < sChar.length && offset < sourceChars.length ) {
-                if( sChar[sPos++] != sourceChars[offset++] ) {
-                    isReplaced = false;
-                    continue;
+    public int maxDistToClosest(int[] seats) {
+        int start = 0, end = 0, maxLen = 0;
+        for(int i = 0 ; i < seats.length ; i++) {
+            if( seats[i] == 0 ) {
+                int s = i, e = i;
+                while( e < seats.length && seats[e] == 0 ) {
+                    e++;
                 }
-            }
-            if(isReplaced) {
-                replaces.put( indexes[i], i );
+                int len = (e - s + 1) / 2;
+                if( s == 0 || e == seats.length) len = e - s;
+                
+                if( len > maxLen ) {
+                    start = s;
+                    end = e;
+                    maxLen = len;
+                }
+                i = e - 1;
             }
         }
         
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < sChar.length ; i++) {
-            if( replaces.containsKey( i ) ) {
-                int index = replaces.get(i);
-                char[] tChar = targets[ index ].toCharArray();
-                int offset = 0;
-                while(offset < tChar.length) {
-                    sb.append( tChar[offset++] );
-                }
-                i += sources[ index ].length() - 1;
-            } else {
-                sb.append( sChar[i] );
-            }
-        }
-        
-        return sb.toString();
+        return maxLen;
     }
 }
