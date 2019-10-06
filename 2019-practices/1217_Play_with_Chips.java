aceeda753f157a0775af6c07b3428050dc052915
zxcvbnius <zxcvbnius@gmail.com>
2019-10-06 12:56:55 +0800

    feat: 1217. Play with Chips

---
+++ 2019-practices/1217_Play_with_Chips.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minCostToMoveChips(int[] chips) {
+        int odd = 0, even = 0;
+        for(int i = 0 ; i < chips.length ; i++) {
+            if( chips[i] % 2 == 0 ) even++;
+            else odd++;
+        }
+        return Math.min(odd, even);
+    }
+}
